58f62f8f8c00b1abaed5a6c810c1efd8ec0eff9c
Kevin Gabriel <maranankevingabriel@gmail.com>
2025-04-26 07:20:13 +0800

    Test

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'https://porchwriters.com/afmams/';  online
+$config['base_url'] = 'https://porchwriters.com/afmams/';
 // $config['base_url'] = 'http://localhost/afmams/'; // local
 
 /*
