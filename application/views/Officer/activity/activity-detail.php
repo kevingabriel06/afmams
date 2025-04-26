3f370a0541c002a801292f8e914c2b4fd34ebd18
Kevin Gabriel <maranankevingabriel@gmail.com>
2025-04-26 13:00:45 +0800

    commit

--- application/views/Officer/activity/activity-detail.php
+++ application/views/Officer/activity/activity-detail.php
@@ -679,7 +679,7 @@
 
 
 
-        <?php if ($activity['organizer'] == 'Student Parliament') : ?>
+        <?php if ($activity['organizer'] == 'Bachelor of Science in Information Systems') : ?>
           <?php if ($activity['status'] == 'Upcoming' || $activity['status'] == 'Ongoing') : ?>
             <?php if ($activity['is_shared'] == 'No') : ?>
               <button id="share" class="btn btn-falcon-default btn-sm me-2" type="button">
