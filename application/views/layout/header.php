b0fd930e459358275c178c269e5fe1070d34fdd1
Kevin Gabriel <maranankevingabriel@gmail.com>
2025-04-26 12:17:13 +0800

    test

--- application/views/layout/header.php
+++ application/views/layout/header.php
@@ -388,7 +388,7 @@
                   <div class="bg-white dark__bg-1000 rounded-2 py-2">
                     <a class="dropdown-item fw-bold text-warning"><span>Officer Account</span></a>
                     <div class="dropdown-divider"></div>
-                    <a class="dropdown-item" href="<?php echo site_url('admin/profile-settings/' . $this->session->userdata('student_id')); ?>">Profile Settings</a>
+                    <a class="dropdown-item" href="<?php echo site_url('officer/profile-settings') ?>">Profile Settings</a>
                     <div class="dropdown-divider"></div>
                     <a class="dropdown-item" href="<?php echo site_url('logout'); ?>">Logout</a>
                   </div>
